ed9e9f9c14c04262be1e4c0e6aab3cf6c91c6a7b
Pablo-Saxoo <pawel.karate@gmail.com>
2025-02-21 16:35:43 +0100

    Simulation light improved

--- custom.js
+++ custom.js
@@ -1,10 +1,13 @@
 import * as THREE from 'three';
 import { STLLoader } from 'three/addons/loaders/STLLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+import { FatLinesBatch } from "./JSSim/FatLinesBatch";
+import { GUI } from "three/examples/jsm/libs/lil-gui.module.min";
+
 
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0xBA7979)
-scene.fog = new THREE.Fog( 'black', 1, 15 );
+scene.background = new THREE.Color(0x999999)
 const camera = new THREE.PerspectiveCamera( 80, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 let cameraTarget = new THREE.Vector3( 0, 0, 0);
@@ -14,24 +17,57 @@ renderer.setSize( window.innerWidth, 700);
 
 document.getElementsByClassName("sym")[0].appendChild( renderer.domElement );
 
+const light1 = new THREE.DirectionalLight( 0xffffff, 4);
+// light.position.setScalar(30);
+// light1.position.set( 8, -30, 10);
+light1.position.set( 0, 0, -10);
+scene.add( light1 );
 
+const light2 = new THREE.DirectionalLight( 0xffffff, 4);
+// light.position.setScalar(30);
+light2.position.set( 0, 0, 10);
+scene.add( light2 );
+
+// camera.add( new THREE.PointLight( 0xffffff, 200 ) );
+
+const controls = new OrbitControls( camera, renderer.domElement );
+controls.addEventListener( 'change', render );
+controls.update();
+
+window.addEventListener( 'resize', onWindowResize );
+
+function onWindowResize() {
+
+	camera.aspect = window.innerWidth / window.innerHeight;
+	camera.updateProjectionMatrix();
+
+	renderer.setSize( window.innerWidth, window.innerHeight );
+
+	render();
+
+}
+
+function render() {
+
+	renderer.render( scene, camera );
+
+}
 
 const stlLoader = new STLLoader();
 
 stlLoader.load( './assets/models/Mouthpiece-alt.STL', function (geometry) {
 
-	const material = new THREE.MeshPhongMaterial( { color: 0xFBD596, specular: 0x494949, shininess: 100 } );
+	const material = new THREE.MeshPhongMaterial( { color: 0xBA7979, specular: 0xBA7979, shininess: 10 } );
+
 	const mesh = new THREE.Mesh( geometry, material );
 	mesh.position.set( 0, -30, 0 );
 	mesh.rotation.set( - Math.PI / 2, 0, 0);
 	mesh.scale.set( 1, 1, 1 );
-	// mesh.castShadow = true;
-	// mesh.receiveShadow = true;
+	mesh.castShadow = true;
+	mesh.receiveShadow = true;
 
 	scene.add( mesh );
 } );
-
-
 
 function animate() {
 
